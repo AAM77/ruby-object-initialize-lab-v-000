233f46c0cd697876e41a10a0630ebc4db1491be3
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-20 15:33:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,12 @@ class Dog
   
   attr_accessor :name, :breed
   
-  def initialize(dog_name, breed)
+  def initialize(dog_name, breed = "Mutt")
     @name = dog_name
     @breed = breed
   end
+  
+  
   
   
   
